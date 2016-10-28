4162ca37dd3bf0f86e929d8c5cb8b3d5c589e599
Alexey Zavyalov <alexey.zavyalov@cinarra.com>
2016-10-28 23:36:54 +0300

    move swagger plugin to platform parent small changes in swagger html add swagger annotations to SettingController

--- user/src/main/java/com/gracelogic/platform/user/controller/SettingController.java
+++ user/src/main/java/com/gracelogic/platform/user/controller/SettingController.java
@@ -8,6 +8,7 @@ import com.gracelogic.platform.web.dto.EmptyResponse;
 import com.gracelogic.platform.web.dto.ErrorResponse;
 import com.gracelogic.platform.web.dto.ValueRequest;
 import com.gracelogic.platform.web.service.LocaleHolder;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -23,39 +24,61 @@ import org.springframework.web.bind.annotation.*;
  */
 @Controller
 @RequestMapping(value = Path.API_SETTING)
+@Api(value = Path.API_SETTING, description = "Базовый контроллер управления пользовательскими настройками",
+        authorizations = @Authorization(value = "MybasicAuth"))
 public class SettingController extends AbstractAuthorizedController {
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private UserService userService;
 
-    @Autowired
-    @Qualifier("userMessageSource")
-    private ResourceBundleMessageSource messageSource;
+  @Autowired
+  @Qualifier("userMessageSource")
+  private ResourceBundleMessageSource messageSource;
 
-    @RequestMapping(value = "/{key}", method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity getUserSetting(@PathVariable(value = "key") String key) {
-        if (getUser() == null) {
-            return new ResponseEntity<ErrorResponse>(new ErrorResponse("auth.NOT_AUTHORIZED", messageSource.getMessage("auth.NOT_AUTHORIZED", null, LocaleHolder.getLocale())), HttpStatus.UNAUTHORIZED);
-        }
-
-        UserSetting userSetting = userService.getUserSetting(getUser().getId(), key);
-        if (userSetting != null) {
-            return new ResponseEntity<UserSettingDTO>(UserSettingDTO.prepare(userSetting), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<ErrorResponse>(new ErrorResponse("setting.NOT_FOUND", messageSource.getMessage("setting.NOT_FOUND", null, LocaleHolder.getLocale())), HttpStatus.BAD_REQUEST);
-        }
+  @ApiOperation(
+          value = "getUserSetting",
+          notes = "Получить настройку по коду"
+  )
+  @ApiResponses({
+          @ApiResponse(code = 200, message = "OK"),
+          @ApiResponse(code = 401, message = "Unauthorized"),
+          @ApiResponse(code = 500, message = "Something exceptional happened")})
+  @RequestMapping(value = "/{key}", method = RequestMethod.GET)
+  @ResponseBody
+  public ResponseEntity getUserSetting(@ApiParam(name = "key", value = "key")
+                                       @PathVariable(value = "key") String key) {
+    if (getUser() == null) {
+      return new ResponseEntity<ErrorResponse>(new ErrorResponse("auth.NOT_AUTHORIZED", messageSource.getMessage("auth.NOT_AUTHORIZED", null, LocaleHolder.getLocale())), HttpStatus.UNAUTHORIZED);
     }
 
-    @RequestMapping(value = "/{key}/save", method = RequestMethod.POST)
-    @ResponseBody
-    public ResponseEntity saveUserSetting(@PathVariable(value = "key") String key,
-                                          @RequestBody ValueRequest request) {
-        if (getUser() == null) {
-            return new ResponseEntity<ErrorResponse>(new ErrorResponse("auth.NOT_AUTHORIZED", messageSource.getMessage("auth.NOT_AUTHORIZED", null, LocaleHolder.getLocale())), HttpStatus.UNAUTHORIZED);
-        }
-
-        userService.updateUserSetting(getUser().getId(), key, request.getValue());
-
-        return new ResponseEntity<EmptyResponse>(EmptyResponse.getInstance(), HttpStatus.OK);
+    UserSetting userSetting = userService.getUserSetting(getUser().getId(), key);
+    if (userSetting != null) {
+      return new ResponseEntity<UserSettingDTO>(UserSettingDTO.prepare(userSetting), HttpStatus.OK);
+    } else {
+      return new ResponseEntity<ErrorResponse>(new ErrorResponse("setting.NOT_FOUND", messageSource.getMessage("setting.NOT_FOUND", null, LocaleHolder.getLocale())), HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @ApiOperation(
+          value = "saveUserSetting",
+          notes = "Сохранить настройку",
+          response = ResponseEntity.class
+  )
+  @ApiResponses({
+          @ApiResponse(code = 200, message = "OK"),
+          @ApiResponse(code = 401, message = "Unauthorized"),
+          @ApiResponse(code = 500, message = "Something exceptional happened")})
+  @RequestMapping(value = "/{key}/save", method = RequestMethod.POST)
+  @ResponseBody
+  public ResponseEntity saveUserSetting(@ApiParam(name = "key", value = "key")
+                                        @PathVariable(value = "key") String key,
+                                        @ApiParam(name = "request", value = "request")
+                                        @RequestBody ValueRequest request) {
+    if (getUser() == null) {
+      return new ResponseEntity<ErrorResponse>(new ErrorResponse("auth.NOT_AUTHORIZED", messageSource.getMessage("auth.NOT_AUTHORIZED", null, LocaleHolder.getLocale())), HttpStatus.UNAUTHORIZED);
+    }
+
+    userService.updateUserSetting(getUser().getId(), key, request.getValue());
+
+    return new ResponseEntity<EmptyResponse>(EmptyResponse.getInstance(), HttpStatus.OK);
+  }
 }
