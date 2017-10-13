aae06e0119dd8592857d5e9ee92e0262b9dffe77
parko <ingvarparko@gmail.com>
2017-10-13 11:35:34 +0300

    [cf] swagger

--- user/src/main/java/com/gracelogic/platform/user/model/UserSetting.java
+++ user/src/main/java/com/gracelogic/platform/user/model/UserSetting.java
@@ -8,11 +8,6 @@ import javax.persistence.*;
 import java.util.Date;
 import java.util.UUID;
 
-/**
- * Author: Igor Parkhomenko
- * Date: 25.06.12
- * Time: 18:30
- */
 @Entity
 @Table(name = JPAProperties.TABLE_PREFIX + "USER_SETTING",
         uniqueConstraints = @UniqueConstraint(columnNames={"USER_ID", "KEY"})
