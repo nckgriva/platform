03ac2cd3c6bee19ec6bdc76535ff3f48ed067b25
parko <ingvarparko@gmail.com>
2017-11-23 17:14:39 +0300

    [nf] elementFieldType in content module

---
+++ content/src/main/java/com/gracelogic/platform/content/model/ElementFieldType.java
@@ -0,0 +1,80 @@
+package com.gracelogic.platform.filestorage.model;
+
+import com.gracelogic.platform.db.JPAProperties;
+import com.gracelogic.platform.db.model.IdObject;
+import com.gracelogic.platform.dictionary.model.Dictionary;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = JPAProperties.TABLE_PREFIX + "STORE_MODE")
+public class StoreMode extends IdObject<UUID> implements Dictionary {
+    @Id
+    @Access(AccessType.PROPERTY)
+    @Column(name = ID)
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @org.hibernate.annotations.Type(type = "pg-uuid")
+    private UUID id;
+
+    @Column(name = CREATED, nullable = false)
+    private Date created;
+
+    @Version
+    @Column(name = CHANGED, nullable = false)
+    private Date changed;
+
+    @Column(name = NAME, nullable = false)
+    private String name;
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public Date getCreated() {
+        return created;
+    }
+
+    @Override
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    @Override
+    public Date getChanged() {
+        return changed;
+    }
+
+    @Override
+    public void setChanged(Date changed) {
+        this.changed = changed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getCode() {
+        return null;
+    }
+
+    @Override
+    public Integer getSortOrder() {
+        return 0;
+    }
+}
---
+++ content/src/main/java/com/gracelogic/platform/content/service/DataConstants.java
@@ -0,0 +1,81 @@
+package com.gracelogic.platform.user.service;
+
+import java.util.UUID;
+
+public class DataConstants {
+
+    public enum AuthCodeTypes {
+        PHONE_VERIFY(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e71")),
+        EMAIL_VERIFY(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e72")),
+        PASSWORD_REPAIR(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e73"));
+        private UUID value;
+
+        AuthCodeTypes(UUID value) {
+            this.value = value;
+        }
+
+        public UUID getValue() {
+            return value;
+        }
+    }
+
+    public enum AuthCodeStates {
+        NEW(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e71")),
+        DELETED(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e72"));
+        private UUID value;
+
+        AuthCodeStates(UUID value) {
+            this.value = value;
+        }
+
+        public UUID getValue() {
+            return value;
+        }
+    }
+
+    public enum UserSettingKey {
+        DAILY_RATE("DAILY_RATE");
+
+        private String value;
+
+        UserSettingKey(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public enum UserApproveMethod {
+        AUTO("AUTO"),
+        MANUAL("MANUAL"),
+        EMAIL_CONFIRMATION("EMAIL_CONFIRMATION"),
+        PHONE_CONFIRMATION("PHONE_CONFIRMATION"),
+        EMAIL_AND_PHONE_CONFIRMATION("PHONE_CONFIRMATION");
+
+        private String value;
+
+        UserApproveMethod(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public enum Forms {
+        USER(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e71"));
+
+        private UUID value;
+
+        Forms(UUID value) {
+            this.value = value;
+        }
+
+        public UUID getValue() {
+            return value;
+        }
+    }
+}
