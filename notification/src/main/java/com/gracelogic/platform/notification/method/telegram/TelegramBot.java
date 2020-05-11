a7c6f6443b643a4290c58b22108a029db0f07080
parko <ingvarparko@gmail.com>
2020-05-11 14:42:10 +0300

    [rf] small rf

---
+++ notification/src/main/java/com/gracelogic/platform/notification/method/telegram/TelegramBot.java
@@ -0,0 +1,78 @@
+package com.gracelogic.platform.notification.method.telegram;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    private String botToken;
+    private String botUsername;
+    private ReplyKeyboardMarkup keyboard;
+
+    private static Logger logger = Logger.getLogger(TelegramBot.class);
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String command = update.getMessage().getText();
+        if (StringUtils.equalsIgnoreCase(command, "/start") || StringUtils.equalsIgnoreCase(command, "/id")) {
+            returnChatId(update);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public TelegramBot(String botToken, String botUsername) {
+        this.botToken = botToken;
+        this.botUsername = botUsername;
+        setKeyboards();
+    }
+
+    private void setKeyboards() {
+        KeyboardRow row = new KeyboardRow();
+        row.add("id");
+
+        List<KeyboardRow> buttonsList = new LinkedList<>();
+        buttonsList.add(row);
+
+        keyboard = new ReplyKeyboardMarkup();
+        this.keyboard.setKeyboard(buttonsList);
+        this.keyboard.setResizeKeyboard(true);
+    }
+
+    private void returnChatId(Update update) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setText(update.getMessage().getChatId().toString());
+        sendMessage.setReplyMarkup(this.keyboard);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            logger.error("Failed to send message", e);
+        }
+    }
+
+    public void sendNotification(String text, String chatId) throws TelegramApiException {
+        SendMessage notificationMessage = new SendMessage();
+        notificationMessage.setText(text);
+        notificationMessage.setChatId(chatId);
+        notificationMessage.setReplyMarkup(this.keyboard);
+        execute(notificationMessage);
+    }
+}
--- notification/src/main/java/com/gracelogic/platform/notification/method/telegram/TelegramNotificationSender.java
+++ notification/src/main/java/com/gracelogic/platform/notification/method/telegram/TelegramNotificationSender.java
@@ -1,11 +1,12 @@
 package com.gracelogic.platform.notification.method.telegram;
 
-import com.gracelogic.platform.notification.bots.telegram.TelegramNotificationBot;
 import com.gracelogic.platform.notification.dto.Content;
 import com.gracelogic.platform.notification.dto.NotificationSenderResult;
 import com.gracelogic.platform.notification.service.DataConstants;
 import com.gracelogic.platform.notification.service.NotificationSender;
 import com.gracelogic.platform.property.service.PropertyService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.ApiContextInitializer;
@@ -20,33 +21,40 @@ public class TelegramNotificationSender implements NotificationSender {
     @Autowired
     private PropertyService propertyService;
 
-    private TelegramNotificationBot telegramNotificationBot;
+    private TelegramBot telegramBot = null;
+
+    private static Logger logger = Logger.getLogger(TelegramNotificationSender.class);
+
 
     @PostConstruct
-    private void initBots(){
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+    private void init(){
+        String telegramBotUsername = propertyService.getPropertyValue("notification:telegram_bot_username");
+        String telegramBotToken = propertyService.getPropertyValue("notification:telegram_bot_token");
+        if (!StringUtils.isEmpty(telegramBotUsername) && !StringUtils.isEmpty(telegramBotToken)) {
+            ApiContextInitializer.init();
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+            telegramBot = new TelegramBot(telegramBotUsername, telegramBotToken);
 
-        String telegramUsername = propertyService.getPropertyValue("notification:telegram_bot_username");
-        String telegramToken = propertyService.getPropertyValue("notification:telegram_bot_token");
-        telegramNotificationBot = new TelegramNotificationBot(telegramUsername, telegramToken);
-        try {
-            telegramBotsApi.registerBot(telegramNotificationBot);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            try {
+                telegramBotsApi.registerBot(telegramBot);
+            } catch (TelegramApiException e) {
+                logger.error("Failed to initialize telegram bot", e);
+            }
         }
-
-
     }
 
     @Override
     public NotificationSenderResult send(String source, String destination, Content content) {
         try {
-            telegramNotificationBot.sendNotification(content.getBody(), destination);
-        } catch (TelegramApiException e) {
+            if (telegramBot == null) {
+                throw new RuntimeException("Telegram bot not initialized");
+            }
+
+            telegramBot.sendNotification(content.getBody(), destination);
+            return new NotificationSenderResult(true, null);
+        } catch (Exception e) {
             return new NotificationSenderResult(false, e.getMessage());
         }
-        return new NotificationSenderResult(true, null);
     }
 
     @Override
