ef707359c4eedf1357f04ca5d5bedca1ae436bed
parko <ingvarparko@gmail.com>
2022-02-11 12:48:17 +0300

    [cf] Removed old values from PaymentSystems enum

--- payment/src/main/java/com/gracelogic/platform/payment/DataConstants.java
+++ payment/src/main/java/com/gracelogic/platform/payment/DataConstants.java
@@ -5,11 +5,7 @@ import java.util.UUID;
 public class DataConstants {
 
     public enum PaymentSystems {
-        MANUAL(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e61")),
-        BANK_IMPORT(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e62")),
-        YANDEX_MONEY(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e71")),
-        SBRF(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e72")),
-        ELECSNET(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e73"));
+        MANUAL(UUID.fromString("54480ce1-00eb-4179-a2b6-f74daa6b9e61")); //Reserved payment system ID
         private UUID value;
 
         PaymentSystems(UUID value) {
